3f764b67765ce166ba4a27886badb71ec1e68f91
Syed-Hanzalah-Ali <syedhanzalahali1062@gmail.com>
2026-05-14 12:11:34 +0500

    pages and routes initial setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export default function App() {
   return (
-    <div className='text-3xl text-blue-600'>App</div>
+    <div className='text-3xl text-blue-600'>
+      navbar
+      <Outlet/>
+    </div>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,48 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import SignUp from './pages/SignUp.jsx'
+import SignIn from './pages/SignIn.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Projects from './pages/Projects.jsx'
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    Component:App,
+    children:[
+      {
+        index:true,
+        Component:Home
+      },
+      {
+        path:"about",
+        Component:About
+      },
+      {
+        path:"dashboard",
+        Component:Dashboard
+      },
+      {
+        path:"projects",
+        Component:Projects
+      },
+      {
+        path:"sign-up",
+        Component:SignUp
+      },
+      {
+        path:"sign-in",
+        Component:SignIn
+      }
+    ]
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router}/>
 )
