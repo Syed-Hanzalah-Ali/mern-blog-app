600292ba9db4fabfb18b04ef003d5d18163f3ece
Syed-Hanzalah-Ali <syedhanzalahali1062@gmail.com>
2026-05-20 16:10:31 +0500

    create a test api route

--- server/src/app.js
+++ server/src/app.js
@@ -2,4 +2,9 @@ import express from "express"
 
 const app=express()
 
+
+// routes
+import userRouter from "./routes/user.route.js"
+
+app.use("/api/v1/users",userRouter);
 export {app}
--- server/src/index.js
+++ server/src/index.js
@@ -4,8 +4,8 @@ import { connectDB } from "./DB/db.config.js";
 
 connectDB()
 .then(()=>{
-    app.listen(3000,()=>{
-        console.log("server is listening");
+    app.listen(process.env.PORT,()=>{
+        console.log("server is listening on port: ",process.env.PORT);
         
     })
 
