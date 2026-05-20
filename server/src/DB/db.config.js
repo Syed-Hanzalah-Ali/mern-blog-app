e867f03d9b4967391779305f48e91ae993348d5f
Syed-Hanzalah-Ali <syedhanzalahali1062@gmail.com>
2026-05-20 15:36:02 +0500

    connect to the database

---
+++ server/src/DB/db.config.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectDB=async ()=>{
+    try {
+        const connectionInstance=await mongoose.connect(process.env.MONGO_URI)
+        console.log(connectionInstance.connection.host);
+    } 
+
+    catch (error) {
+        throw error;
+        process.exit(1);    
+    }
+}
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,16 @@
 import "dotenv/config"
 import { app } from "./app.js";
+import { connectDB } from "./DB/db.config.js";
 
+connectDB()
+.then(()=>{
+    app.listen(3000,()=>{
+        console.log("server is listening");
+        
+    })
 
-app.listen(3000,()=>{
-    console.log("server is listening");
+})
+.catch((error)=>{
+    console.log("MongoDB connection failed: ",error);
     
 })
