8f3aafb2e400b485db0eac394977f4b487301e88
Syed-Hanzalah-Ali <syedhanzalahali1062@gmail.com>
2026-05-20 15:54:40 +0500

    setup handler error and response functions

---
+++ server/src/utils/asyncHandler.js
@@ -0,0 +1,15 @@
+export const asyncHandler=(func)=>{
+    return async(req,res,next)=>{
+        try {
+            await func(req,res,next);
+        } 
+        catch (error) {
+            console.log("getting here",error);
+            
+            res.status(error.statusCode||500).json({
+                success:false,
+                message:error.message
+            })
+        }
+    }
+}
