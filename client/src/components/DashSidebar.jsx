f7242638272ad3fedb8d3efcd23b2cd6286713f6
Syed-Hanzalah-Ali <syedhanzalahali1062@gmail.com>
2026-06-07 13:08:36 +0500

    complete sidebar of the dashboard

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Sidebar, SidebarItem, SidebarItemGroup, SidebarItems} from "flowbite-react"
+import {HiUser,HiLogout} from "react-icons/hi"
+import { Link } from 'react-router-dom'
+export default function DashSidebar({tab}) {
+  return (
+    <Sidebar className='w-full md:w-56'>
+
+      <SidebarItems>
+
+        <SidebarItemGroup>
+
+          <Link to="/dashboard?tab=profile">
+            <SidebarItem icon={HiUser} active={tab==="profile"} label='User' labelColor='dark'>
+              Profile
+            </SidebarItem>
+          
+          </Link>
+
+
+          <SidebarItem icon={HiLogout} className='cursor-pointer'>
+            Signout
+          </SidebarItem>
+
+        </SidebarItemGroup>
+
+      </SidebarItems>
+    </Sidebar>
+  )
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import {useLocation} from "react-router-dom" 
+import DashSidebar from '../components/DashSidebar'
+import DashProfile from '../components/DashProfile'
 export default function Dashboard() {
+  const path=useLocation()
+  const [tab ,setTab]=useState('')
+  
+  // whenever the path search changes means get the search params to render that sub section of dashboard
+  useEffect(()=>{
+    // extract all the search params from URL
+    const urlParams= new URLSearchParams(path.search)
+    
+    // get the search params value you want 
+    const tabParam=urlParams.get("tab")
+    // console.log(tabParam);
+    if(tabParam){
+      setTab(tabParam)
+    }
+    
+  },[path.search])
   return (
-    <div>Dashboard</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+
+      {/* sidebar */}
+      <div className='md:w-56'>
+        <DashSidebar tab={tab}/>
+      </div>
+
+
+      {/* right side profile */}
+      {tab==="profile"&&<DashProfile/>}
+    </div>
   )
 }
