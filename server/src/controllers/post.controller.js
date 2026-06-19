3d838312b7b08c619cd34f9c23aff9ef60c2702d
Syed-Hanzalah-Ali <syedhanzalahali1062@gmail.com>
2026-06-19 17:05:11 +0500

    create get post API route

--- server/src/controllers/post.controller.js
+++ server/src/controllers/post.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Post } from "../models/post.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import mongoose from "mongoose";
 
 export const createPost=asyncHandler(async(req,res)=>{
     if(!req.user.isAdmin){
@@ -36,4 +37,67 @@ export const createPost=asyncHandler(async(req,res)=>{
     return res.status(201).json(
         new ApiResponse(200,newPost,"new post created successfully")
     )
+})
+
+export const getPosts=asyncHandler(async(req,res)=>{
+    const startIndex=parseInt(req.query.startIndex) ||0
+    const limit=parseInt(req.query.limit)||9
+    const order=req.query.order==='asc'?1:-1
+
+    const {userId,postId,search,category,slug}=req.query
+
+    const matchStage={
+        ...(userId&&{author:userId}),
+        ...(postId&&{_id:postId}),
+        ...(category&&{category}),
+        ...(slug&&{slug}),
+        ...(search && {
+            $or: [
+                { title: { $regex: search, $options: 'i' } },
+                { content: { $regex: search, $options: 'i' } }
+            ]
+        })
+    }
+
+    const posts=await Post.aggregate([
+        {
+            $match:matchStage,
+           
+        },
+        {
+            $sort:{
+                updatedAt:order
+            }
+        },
+        {
+            $skip:startIndex
+        },
+        {
+            $limit:limit
+        }
+        
+        
+    ])
+    const totalPosts=await Post.countDocuments()
+    // console.log("total: ",totalPosts);
+    
+    // last month
+    const now=new Date()
+    const oneMonthAgo=new Date(
+        now.getFullYear(),
+        now.getMonth()-1,
+        now.getDate()
+    )
+    // console.log(now);
+    
+    // console.log(oneMonthAgo);
+
+    const lastMonthTotalPost=await Post.countDocuments({
+      createdAt:{$gte:oneMonthAgo}  
+    })
+    
+
+    return res.status(200).json(
+        new ApiResponse(200,{posts,totalPosts,lastMonthTotalPost},"post fetched successfully")
+    );
 })
--- server/src/routes/post.route.js
+++ server/src/routes/post.route.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, getPosts } from "../controllers/post.controller.js";
 
 const router=Router()
 
-router.use(verifyJWT)
+// router.use(verifyJWT)
 
-router.route("/create").post(upload.single('image'),createPost)
+router.route("/create").post(verifyJWT,upload.single('image'),createPost)
 
+router.route("/getposts").get(getPosts)
 export default router
