a3e769a3e42dddb454b8a1ef66e64b2ba420a489
Syed-Hanzalah-Ali <syedhanzalahali1062@gmail.com>
2026-06-20 12:55:58 +0500

    add show more functionality to the posts result of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -7,6 +7,39 @@ export default function DashPosts() {
   const {currentUser}=useSelector((state)=>state.user)
 
   const [userPosts,setUserPosts]=useState([])
+  const [showMore,setShowMore]=useState(true)
+
+  async function handleShowMore(){
+    const startIndex=userPosts.length
+    // console.log(startIndex);
+
+    try {
+      const response= await fetch(`/api/v1/posts/getposts?userId=${currentUser._id}&startIndex=${startIndex}`)
+      const result=await response.json()
+      if(result.success===true){
+        // console.log(result.data.posts);
+        setUserPosts((prev)=>[...prev,...result.data.posts])
+
+        if(result.data.posts.length<9){
+            // console.log("length is less than 9");
+            setShowMore(false)
+        }
+        
+      }
+    } 
+    catch (error) {
+      console.log(error.message);
+        
+    }
+
+  
+    
+  }
+  // console.log(userPosts.length);
+  
+  // console.log(showMore);
+  
+
   useEffect(()=>{
     async function getPosts(){
       try {
@@ -15,6 +48,10 @@ export default function DashPosts() {
         // console.log(result);
         if(result.success===true){
           setUserPosts(result.data.posts)
+          if(result.data.posts.length<9){
+            // console.log("length is less than 9");
+            setShowMore(false)
+          }
 
         }
       } 
@@ -32,57 +69,68 @@ export default function DashPosts() {
   // console.log(userPosts);
   
   return (
-    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar 
+    <div className='w-full table-auto overflow-x-scroll md:mx-auto p-3 scrollbar 
     scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser?.isAdmin&&userPosts.length>0?(
-        <Table hoverable className='shadow-md'>
+        <>
+          <Table hoverable className='shadow-md'>
 
-          <TableHead>
-            <TableRow>
-              <TableHeadCell>Date updated</TableHeadCell>
-              <TableHeadCell>Post image</TableHeadCell>
-              <TableHeadCell>Post title</TableHeadCell>
-              <TableHeadCell>Category</TableHeadCell>
-              <TableHeadCell>Delete</TableHeadCell>
-              <TableHeadCell>Edit</TableHeadCell>
-              
-            </TableRow>
-          </TableHead>
+            <TableHead>
+              <TableRow>
+                <TableHeadCell>Date updated</TableHeadCell>
+                <TableHeadCell>Post image</TableHeadCell>
+                <TableHeadCell>Post title</TableHeadCell>
+                <TableHeadCell>Category</TableHeadCell>
+                <TableHeadCell>Delete</TableHeadCell>
+                <TableHeadCell>Edit</TableHeadCell>
+                
+              </TableRow>
+            </TableHead>
 
-          <TableBody className='divide-y'>
-            {userPosts.map((post,index)=>{
-              return(
-                <TableRow key={index} className='bg-white dark:border-gray-700 dark:bg-gray-800'>
-                  <TableCell>{new Date(post.updatedAt).toLocaleDateString()}</TableCell>
-                  <TableCell>
-                    <Link to={`/post/${post._id}`}>
-                      <img src={post.image} alt='post.image' className='w-20 h-10 object-cover bg-gray-500'/>
+            <TableBody className='divide-y'>
+              {userPosts.map((post,index)=>{
+                return(
+                  <TableRow key={index} className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                    <TableCell>{new Date(post.updatedAt).toLocaleDateString()}</TableCell>
+                    <TableCell>
+                      <Link to={`/post/${post._id}`}>
+                        <img src={post.image} alt='post.image' className='w-20 h-10 object-cover bg-gray-500'/>
 
-                    </Link>
-                  </TableCell>
-                  <TableCell>
-                    <Link className='font-medium text-gray-900 dark:text-white ' 
-                    to={`/post/${post._id}`}>
-                      {post.title}
-                    </Link>
+                      </Link>
                     </TableCell>
-                  <TableCell>{post.category}</TableCell>
+                    <TableCell>
+                      <Link className='font-medium text-gray-900 dark:text-white ' 
+                      to={`/post/${post._id}`}>
+                        {post.title}
+                      </Link>
+                      </TableCell>
+                    <TableCell>{post.category}</TableCell>
 
-                  <TableCell>
-                    <span className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span>
-                  </TableCell>
+                    <TableCell>
+                      <span className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span>
+                    </TableCell>
 
-                  <TableCell>
-                    <Link className='text-teal-500 hover:underline' to={`/update-post/${post._id}`}>
-                      <span>Edit</span>
-                    </Link>
-                  </TableCell>
-                </TableRow>
-              )
-            })}
-          </TableBody>
+                    <TableCell>
+                      <Link className='text-teal-500 hover:underline' to={`/update-post/${post._id}`}>
+                        <span>Edit</span>
+                      </Link>
+                    </TableCell>
+                  </TableRow>
+                )
+              })}
+            </TableBody>
 
-        </Table>
+          </Table>
+
+          {
+            showMore&&(
+              <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+                show more
+              </button>
+            )
+          }
+        </>
+
       )
       :(
         <p>You have no post yet</p>
