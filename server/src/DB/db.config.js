e191d960e436896cb8663e9ffc9164a3a8972ba5
Syed-Hanzalah-Ali <syedhanzalahali1062@gmail.com>
2026-06-28 20:14:28 +0500

    add pre installation

--- server/src/DB/db.config.js
+++ server/src/DB/db.config.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB=async ()=>{
     try {
         const connectionInstance=await mongoose.connect(process.env.MONGO_URI)
-        console.log(connectionInstance.connection.host);
+        // console.log(connectionInstance.connection.host);
     } 
 
     catch (error) {
--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,10 @@
 import express from "express"
 import cookieParser from "cookie-parser";
 const app=express()
+import path from "path";
+
+const __dirname=path.resolve()
+// console.log(__dirname);
 
 app.use(express.json());
 
@@ -15,5 +19,13 @@ app.use("/api/v1/users",userRouter);
 app.use("/api/v1/auth",authRouter);
 app.use("/api/v1/posts",postRouter);
 app.use("/api/v1/comments",commentRouter);
+
+//serve react build- run the index.html 
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+// whatever the path except we define above runs under index.html
+app.use( (req, res) => {
+  res.sendFile(path.join(__dirname, "client","dist", "index.html"));
+});
 
 export {app}
